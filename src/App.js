cc78fff3ec90f17636291c5c6469f6e75f88cc36
huydeve <haihuy129@gmail.com>
2022-06-14 13:28:19 +0700

    Component part 1

--- src/App.js
+++ src/App.js
@@ -4,28 +4,19 @@ import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
   render() {
     return (
-      <GoogleOAuthProvider clientId="960591633064-bf9k86c616cfe1fbvkod9cd2r0qg6orl.apps.googleusercontent.com">
         <div className="App">
           <Navbar dark color="primary ">
             <div className="container">
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
           </Navbar>
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-          ;
+          <Menu/>
         </div>
-      </GoogleOAuthProvider>
     );
   }
 }
